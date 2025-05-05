5931e8d9e52af1cff0d14ed79f8914c546b6875d
Deyan Ilkov <deo08mine@gmail.com>
2025-05-05 15:20:15 +0300

    Second commit (Tehnically first)

---
+++ EGNValidationWeb/Program.cs
@@ -0,0 +1,14 @@
+using EGNValidation;
+using EGNValidationWeb;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<IEGNValidator, Validation>();
+
+await builder.Build().RunAsync();
